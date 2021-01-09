b094ffcdc350b5d76d859b737786a07715996909
Alexandr Khvostov <san987873@yandex.ru>
2021-01-09 15:32:31 +0300

    Add Dockerfile

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker rm ff220v/smptsa
-docker run ff220v/smptsa --name ff220v/smptsa
+docker run ff220v/smptsa -p 8050:8050 --name ff220v/smptsa
--- src/main.py
+++ src/main.py
@@ -2,4 +2,4 @@ from src.app import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=8050, debug=True)
