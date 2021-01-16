27c73a969a78a209e53d45cfb2603858dec0f86e
Ekaterina <eslutskaya@gmail.com>
2021-01-16 21:11:18 +0300

    Ошибка: tuple index out of range в остальном должен работатьь

--- src/backend.py
+++ src/backend.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
 from functools import partial
 import pandas as pd
+from pandas import concat
+from numpy import asarray
 import numpy as np
 from pandas import DataFrame
 import statsmodels.api as sm
@@ -54,11 +56,35 @@ def moving_avg_forecast(df: DataFrame, prepared_df: DataFrame, prediction_step_l
 
 
 def xgb_model(df: DataFrame, prepared_df: DataFrame, prediction_step_length: timedelta, feature_column: list):
-    df_series = pd.Series(df[df[feature_column]<len(df[feature_column])].array, df[df['time_']<len(df['time_'])])
-    df_series = df_series.resample(Timedelta(prediction_step_length)).ffill()
+    #df_series = pd.Series(df[df[feature_column]<len(df[feature_column])].values, df[df['time_']<len(df['time_'])].astype(np.int64))
+    #df_series = df_series.resample(Timedelta(prediction_step_length)).ffill()
 
-    df_time = np.array(range(len(df_series)))
-    prediction_time = np.array(range(len(df_series), len(prepared_df['time_']) + len(df_series)))
+    #df_time = np.array(range(len(df_series)))
+    def series_to_supervised (data, n_in=1, n_out=1, dropnan=True):
+        n_vars = 1 if type(data) is list else data.shape[1]
+        dfr = DataFrame(data)
+        cols = list()
+        # input sequence (t-n, ... t-1)
+        for i in range(n_in, 0, -1):
+            cols.append(dfr.shift(i))
+        # forecast sequence (t, t+1, ... t+n)
+        for i in range(0, n_out):
+            cols.append(dfr.shift(-i))
+        # put it all together
+        agg = concat(cols, axis=1)
+        # drop rows with NaN values
+        if dropnan:
+            agg.dropna(inplace=True)
+        return agg.values
+
+
+    values =df.values
+    train = series_to_supervised(values, n_in=243)
+    trainX, trainy = train[:, -1], train[:, -1]
+    prepared_df.drop(prepared_df.tail(1).index, inplace=True)
+    values =prepared_df.values
+    prediction = series_to_supervised(values, n_in=len(prepared_df['time_']))
+    predictionx =prediction[:, -1]
 
     model = XGBRegressor(
         max_depth=5,
@@ -70,8 +96,9 @@ def xgb_model(df: DataFrame, prepared_df: DataFrame, prediction_step_length: tim
         seed=42
     )
 
-    model.fit(np.array(df_series), df_time)
-    prepared_df[feature_column] = model.predict(prediction_time)
+    model.fit(trainX, trainy)
+
+    prepared_df[feature_column] = model.predict(predictionx)
     return prepared_df
 
 
@@ -101,5 +128,5 @@ prediction_methods_map = {
 
 if __name__ == "__main__":
     source_data = pd.read_csv("Train.csv")
-    result = predict(source_data, "XGB", 100, timedelta(hours=1), ["Count"], ["Datetime"], None)
-    print('')
+    #result = predict(source_data, "XGB", 100, timedelta(hours=1), ["Count"], ["Datetime"], None)
+    print(source_data.tail(5))
